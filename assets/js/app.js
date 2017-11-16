b58f0d87c3d1775f81862683ec8eee89ca650570
Todd Croak-Falen <toddcf@gmail.com>
2017-11-15 22:50:31 -0800

    Correct answer now turns all squares to the correct color.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,11 +10,14 @@ var colors = [
 
 // Loop through all squares and assign a color to be each one's background:
 var squares = document.querySelectorAll( ".square" );
-var pickedColor = colors[ 3 ];
+var pickedColor = pickColor();
 
 // Set the displayed RGB numbers in the header:
 var colorDisplay = document.getElementById( "colorDisplay" );
 colorDisplay.textContent = pickedColor;
+
+// Message Display:
+var messageDisplay = document.getElementById( "message" );
 
 for ( var i = 0; i < squares.length; i++ ) {
 	
@@ -29,11 +32,30 @@ for ( var i = 0; i < squares.length; i++ ) {
 
 		// Compare color to pickedColor:
 		if ( clickedColor === pickedColor ) {
-			alert( "Correct!" );
+			// Display message:
+			messageDisplay.textContent = "Correct!";
+			changeColors( clickedColor );
 		}
 		else {
-			alert( "Wrong . . ." );
+			// Match the square's bg color to the body's bg color:
+			this.style.backgroundColor = "#232323";
+			// Display message:
+			messageDisplay.textContent = "Incorrect - Try Again";
 		}
-		
+
 	});
 }
+
+function changeColors( color ) {
+
+	for ( var i = 0; i < squares.length; i++ ) {
+		squares[ i ].style.backgroundColor = color;
+	}
+
+};
+
+function pickColor () {
+	// Choose random number between 1 and the number of colors in the array (3 for easy, 6 for hard):
+	var random = Math.floor( Math.random() * colors.length );
+	return colors[ random ];
+};
