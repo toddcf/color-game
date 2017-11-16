165c6152693bfdb815326bb51789c9fbd08b98bb
Todd Croak-Falen <toddcf@gmail.com>
2017-11-15 20:23:25 -0800

    Passed the picked color into the header display.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,11 @@ var colors = [
 
 // Loop through all squares and assign a color to be each one's background:
 var squares = document.querySelectorAll( ".square" );
+var pickedColor = colors[ 3 ];
+
+// Set the displayed RGB numbers in the header:
+var colorDisplay = document.getElementById( "colorDisplay" );
+colorDisplay.textContent = pickedColor;
 
 for ( var i = 0; i < squares.length; i++ ) {
 	squares[ i ].style.backgroundColor = colors[ i ];
